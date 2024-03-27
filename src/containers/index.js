1cde4139909f054d00710450eb5689246ada7015
Marshal-Pfluger <MarshalPfluger7@gmail.com>
2024-03-27 15:27:37 -0500

    created resume Link, need to update with cloudfront.

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,5 +2,4 @@ export { default as Projects } from '../containers/projects/Projects';
 export { default as Resume } from './resume/Resume';
 export { default as Footer } from './footer/Footer';
 export { default as Header } from './header/Header';
-export { default as Skills } from './skills/Skills';
 export { default as About } from './about/About';
--- src/containers/resume/Resume.jsx
+++ src/containers/resume/Resume.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './resume.css';
+import resume from '../../assets/resume.png';
 
 const Resume = () => {
   return (
     <div>
-      Resume
+      <div className='marshalPfluger__resume'>
+        <h1>Resume</h1>
+        <div className='marshalPfluger__resume-grid'>
+          <div className='resume-item' onClick={() => window.open('https://github.com/Marshal-Pfluger', '_blank')}>
+            <img src={resume} alt="resume" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
